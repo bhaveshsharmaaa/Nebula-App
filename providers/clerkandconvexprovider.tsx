2137c1cab8a46d9b6284d7e0a03a871ab72b08bd
bhaveshsharmaaa <bhaveshsharma7456@gmail.com>
2025-06-05 03:01:31 +0530

    saved changes

--- providers/clerkandconvexprovider.tsx
+++ providers/clerkandconvexprovider.tsx
@@ -2,7 +2,6 @@ import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import React from "react";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
